9dad7d1311243c16a446a8ff097b8a31204783df
Alexander_Baranov <Sti_ll@mail.ru>
2024-07-29 15:47:13 +0300

    add new logics to class Category/Product

--- src/category.py
+++ src/category.py
@@ -6,6 +6,16 @@ class Category:
         """Инициализация класса с атрибутами категории товара"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         self.category_count += 1
         self.product_count += len(products)
+
+    def add_product(self, new_products):
+        self.__products.append(new_products)
+        self.product_count += 1
+
+    @property
+    def products(self):
+        for product in self.__products:
+            print(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return
--- src/product.py
+++ src/product.py
@@ -4,5 +4,24 @@ class Product:
         """Инициализация класса с атрибутами продукта"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, params_product: dict):
+        cls.name = params_product["name"]
+        cls.description = params_product["description"]
+        cls.price = params_product["price"]
+        cls.quantity = params_product["quantity"]
+        return cls
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, prices):
+        if prices <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = prices
