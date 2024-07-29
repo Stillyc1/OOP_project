475cf2eaa7ad35db7ce6cf1ce72a49707983bc8f
Alexander_Baranov <Sti_ll@mail.ru>
2024-07-29 17:45:00 +0300

    add new tests for modul category/product

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,7 @@
+from src.product import Product
+from unittest.mock import patch
+
+
 def test_init_product(product1, product2):
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product1.description == "256GB, Серый цвет, 200MP камера"
@@ -8,3 +12,18 @@ def test_init_product(product1, product2):
     assert product2.description == "512GB, Gray space"
     assert product2.price == 210000.0
     assert product2.quantity == 8
+
+
+def test_create_product(new_test_product):
+    test_product = Product.new_product(new_test_product)
+
+    assert test_product.name == "Samsung Galaxy S23 Ultra"
+    assert test_product.description == "256GB, Серый цвет, 200MP камера"
+    assert test_product.price == 180000.0
+    assert test_product.quantity == 5
+
+
+def test_product_setter(capsys, product_test_setter):
+    product_test_setter.price = 500
+    message = capsys.readouterr()
+    assert message.out == "Вы точно хотите понизить цену с 180000.0 до 500? y/n\n\n"
